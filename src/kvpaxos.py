1ef9cc26589f4d4f7ad5addc0f28ecdaa4bfebf0
Hanpeng <lhp.polo@gmail.com>
2016-06-22 11:43:59 -0700

    merge kvpaxos

--- src/kvpaxos.py
+++ src/kvpaxos.py
@@ -1,10 +1,11 @@
 import BaseHTTPServer
 import xmlrpclib
+import threading
 from threading import Thread
 from threading import Lock
 from urlparse import urlparse, parse_qs
 from SimpleXMLRPCServer import SimpleXMLRPCServer, SimpleXMLRPCRequestHandler
-
+import random
 from util import *
 from store import Store
 import sys
@@ -21,7 +22,7 @@ class _RPCFuncs:  # Reused LPH's structure
     # print 'server numbered ' + str(self.server.number) + ' responding to proposed lead'
     if self.server.alive is False:
       return False, "", -3, -1
-    with self.server.seqLock:
+    with self.server.seqLock.read_access:
       while (self.server.minNum + len(self.server.listValue) <= seq):
         self.server.listValue.append("")
         self.server.listRoundNum.append(-1)
@@ -53,7 +54,7 @@ class _RPCFuncs:  # Reused LPH's structure
   def response_to_proposed_value(self, seq, roundNum, proposedVal): # Stage 2b in the slides
     if self.server.alive is False:
       return False, -1
-    with self.server.seqLock:    # A lock here is necessary
+    with self.server.seqLock.read_access:    # A lock here is necessary
       if (roundNum < self.server.listRoundNum[seq-self.server.minNum]):
       # the round number is old
         return False, self.server.listKnownMin[self.server.number]
@@ -63,7 +64,7 @@ class _RPCFuncs:  # Reused LPH's structure
   def response_to_decision(self, seq, decidedVal):  # Stage 3 in the slides
     # Maybe adding some assertations?
     if self.server.alive is True:
-      with self.server.seqLock:
+      with self.server.seqLock.read_access:
         self.server.listValue[seq-self.server.minNum] = decidedVal
       return True
     else :
@@ -79,7 +80,7 @@ class _RPCFuncs:  # Reused LPH's structure
 def threadedDone(server):
   if server.alive is False:
     return
-  with server.seqLock:  # the whole session should be wrapped in locks
+  with server.seqLock.write_access:  # the whole session should be wrapped in locks
     newMinNum = min(server.listKnownMin)
     dif = newMinNum - server.minNum
     if (dif < 0):
@@ -93,14 +94,16 @@ def threadedDone(server):
         del server.listPendingToLead[0]
 def threadedMaintainence(server):
   while (True):
-    time.sleep(2)  # number of seconds slept over next check, I randomly picked "2" without further examination. In reality, this number should depend on the number of servers as well as network condition
+    time.sleep(random.uniform(0.5,1))  # number of seconds slept over next check, I randomly picked "2" without further examination. In reality, this number should depend on the number of servers as well as network condition
     if server.alive is False:
       continue
-    with server.seqLock:
+    with server.seqLock.read_access:
       for i in range(len(server.listValue)):  # suppose this is executed for len(server.listValue) times
         if (server.listValue[i] == "" and server.listPendingValue[i] != ""):
-          client_thread('tStart' + str(server.number) + ' ' 
+          print "I'm working!!!!!!!!!!!"
+          client_thread('tStart' + str(server.number) + ' '
               + str(i + server.minNum) + ' by maintainence', threadedStart, server, i + server.minNum).start()
+          time.sleep(random.uniform(0.1,0.8))
 def threadedResurrect(server):
   for i in range(0, server.amount):
     if server.checkListServer(i) is False:
@@ -111,7 +114,7 @@ def threadedResurrect(server):
       continue
     if remoteMin > server.listKnownMin[i]:
       server.listKnownMin[i] = remoteMin
-    with server.seqLock:
+    with server.seqLock.read_access:
       while (server.minNum + len(server.listValue) - 1 < remoteMax):
         server.listValue.append("")
         server.listRoundNum.append(-1)
@@ -132,8 +135,8 @@ def threadedResurrect(server):
 def threadedServerForever(server):
   server.serve_forever()
 def threadedStart(server, seq):
-  with server.seqLock:
-    if (server.listPendingToLead[seq - server.minNum] > 0 or server.alive == False): 
+  with server.seqLock.read_access:
+    if (server.listPendingToLead[seq - server.minNum] > 0 or server.alive == False):
       return  # full fix
     server.listPendingToLead[seq - server.minNum] = 2
     roundNum = server.listRoundNum[seq - server.minNum] + 1
@@ -153,13 +156,6 @@ def threadedStart(server, seq):
         continue
       # print "Decided to connect to server " + str(i)
       try:
-        """
-        returnVal = server.listServer[i].response_to_proposed_lead(seq, roundNum)
-        result = returnVal[0]
-        prePropose = returnVal[1]
-        returnRoundNum = returnVal[2]
-        individualMin = returnVal[3]
-        """
         result, prePropose, returnRoundNum, individualMin = server.listServer[i].response_to_proposed_lead(seq, roundNum)
       except Exception, e:
         # print "Exception returned from server " + str(i)
@@ -190,7 +186,7 @@ def threadedStart(server, seq):
             highestPrevRound = returnRoundNum
       else:
         if (prePropose != ""):
-          with server.seqLock:
+          with server.seqLock.read_access:
             server.listValue[seq - server.minNum] = prePropose
             server.listPendingToLead[seq - server.minNum] = 0
           return
@@ -207,13 +203,13 @@ def threadedStart(server, seq):
             flag = True
             break
     if server.alive is False:
-      with server.seqLock:
+      with server.seqLock.read_access:
         server.listPendingToLead[seq - server.minNum] = 0
         return
-  with server.seqLock:
-    if ((trueReply + 1) * 2 <= server.amount):  
+  with server.seqLock.read_access:
+    if ((trueReply + 1) * 2 <= server.amount):
       server.listPendingToLead[seq - server.minNum] = 0
-      return 
+      return
     else:
       server.listPendingToLead[seq - server.minNum] = 1
     # I'm OK to lead
@@ -228,6 +224,7 @@ def threadedStart(server, seq):
   trueReply = 0
   if proposedVal is "":
     assert False, "not reached"
+    print "Serious trouble!!!!!!!!!!!!!!!!!!"
   for i in range(0, server.amount):
     """
     if (i == server.number):
@@ -248,18 +245,19 @@ def threadedStart(server, seq):
       trueReply += 1
     if (individualMin > server.listKnownMin[i]):  # some remote server sends his Done message
       # On a second thought I think no lock is fine here
-      if server.listKnownMin[i] is server.minNum:
-        server.listKnownMin[i] = individualMin
-        tDone = client_thread('tDone' + str(server.number) + ' ' + str(individualMin), threadedDone, serverd)
-        tDone.start()
-      else:
-        server.listKnownMin[i] = individualMin
-  if ((trueReply + 1) * 2 <= server.amount or server.alive == False):  
-    with server.seqLock:
+      with server.seqLock:
+        if server.listKnownMin[i] is server.minNum:
+          server.listKnownMin[i] = individualMin
+          tDone = client_thread('tDone' + str(server.number) + ' ' + str(individualMin), threadedDone, serverd)
+          tDone.start()
+        else:
+          server.listKnownMin[i] = individualMin
+  if ((trueReply + 1) * 2 <= server.amount or server.alive == False):
+    with server.seqLock.read_access:
       server.listPendingToLead[seq - server.minNum] = 0
-      return 
+      return
   # It has been decided
-  with server.seqLock:
+  with server.seqLock.read_access:
     server.listValue[seq - server.minNum] = proposedVal
   for i in range(server.amount):
     """
@@ -272,9 +270,9 @@ def threadedStart(server, seq):
       server.listServer[i].response_to_decision(seq, proposedVal)
     except Exception, e:
       continue  # in case remote server is unreachable, continue
-  with server.seqLock:
+  with server.seqLock.read_access:
     server.listPendingToLead[seq - server.minNum] = 0
-  return 
+  return
 class Paxos:
   def __init__ (self):
     self.alive = True
@@ -289,7 +287,7 @@ class Paxos:
     self.listPendingToLead = []
     self.minNum = 0
     #self.listLocks = []  # Locks for different sequence numbers
-    self.seqLock = Lock()
+    self.seqLock = RWLock()
   def checkListServer(self, num):
     if num is self.number:
       return False
@@ -355,8 +353,8 @@ class Paxos:
     return True
   def start(self, seq, value):
     while self.alive is False:
-      time.sleep(2)# Modified at GY's request
-    with self.seqLock:
+      time.sleep(1)# Modified at GY's request
+    with self.seqLock.write_access:
       while (self.minNum + len(self.listValue) <= seq):
         self.listValue.append("")
         self.listRoundNum.append(-1)
@@ -372,7 +370,7 @@ class Paxos:
   def min(self):
     return self.minNum
   def max(self):
-    with self.seqLock:
+    with self.seqLock.read_access:
       return self.minNum + len(self.listValue) - 1
   def kill(self):
     self.alive = False
@@ -382,11 +380,8 @@ class Paxos:
     tResurrect.start()
     if wait_bool is True:
       tResurrect.join()
+
   def done(self, seq):
-    """
-    if self.alive is False:
-      return False
-    """
     if (self.number == -1):
       return False
     if (seq > self.listKnownMin[self.number]):
@@ -398,10 +393,11 @@ class Paxos:
         self.listKnownMin[self.number] = seq
     return True
   def status(self, seq):
-    with self.seqLock:
+    with self.seqLock.read_access:
       if (seq < self.minNum) :
         # return False, ""    # Modified at GY's request
         return True, None
+      print "access"
       if (seq - self.minNum >= len(self.listValue)) :
         return False, '1'
       if (self.listValue[seq - self.minNum] == "") :
@@ -420,3 +416,115 @@ class client_thread(Thread):  # Reused from Google's test.py
   def run(self):
     result = self.run_func(*self.args)
     self.result = result
+
+
+class RWLock:
+    '''Non-reentrant write-preferring rwlock.'''
+    DEBUG = 0
+
+    def __init__(self):
+        self.lock = threading.Lock()
+
+        self.active_writer_lock = threading.Lock()
+        # The total number of writers including the active writer and
+        # those blocking on active_writer_lock or readers_finished_cond.
+        self.writer_count = 0
+
+        # Number of events that are blocking on writers_finished_cond.
+        self.waiting_reader_count = 0
+
+        # Number of events currently using the resource.
+        self.active_reader_count = 0
+
+        self.readers_finished_cond = threading.Condition(self.lock)
+        self.writers_finished_cond = threading.Condition(self.lock)
+
+        class _ReadAccess:
+            def __init__(self, rwlock):
+                self.rwlock = rwlock
+            def __enter__(self):
+                self.rwlock.acquire_read()
+                return self.rwlock
+            def __exit__(self, type, value, tb):
+                self.rwlock.release_read()
+        # support for the with statement
+        self.read_access = _ReadAccess(self)
+
+        class _WriteAccess:
+            def __init__(self, rwlock):
+                self.rwlock = rwlock
+            def __enter__(self):
+                self.rwlock.acquire_write()
+                return self.rwlock
+            def __exit__(self, type, value, tb):
+                self.rwlock.release_write()
+        # support for the with statement
+        self.write_access = _WriteAccess(self)
+
+        if self.DEBUG:
+            self.active_readers = set()
+            self.active_writer = None
+
+    def acquire_read(self):
+        with self.lock:
+            if self.DEBUG:
+                me = threading.currentThread()
+                assert me not in self.active_readers, 'This thread has already acquired read access and this lock isn\'t reader-reentrant!'
+                assert me != self.active_writer, 'This thread already has write access, release that before acquiring read access!'
+                self.active_readers.add(me)
+            if self.writer_count:
+                self.waiting_reader_count += 1
+                self.writers_finished_cond.wait()
+                # Even if the last writer thread notifies us it can happen that a new
+                # incoming writer thread acquires the lock earlier than this reader
+                # thread so we test for the writer_count after each wait()...
+                # We also protect ourselves from spurious wakeups that happen with some POSIX libraries.
+                while self.writer_count:
+                    self.writers_finished_cond.wait()
+                self.waiting_reader_count -= 1
+            self.active_reader_count += 1
+
+    def release_read(self):
+        with self.lock:
+            if self.DEBUG:
+                me = threading.currentThread()
+                assert me in self.active_readers, 'Trying to release read access when it hasn\'t been acquired by this thread!'
+                self.active_readers.remove(me)
+            assert self.active_reader_count > 0
+            self.active_reader_count -= 1
+            if not self.active_reader_count and self.writer_count:
+                self.readers_finished_cond.notifyAll()
+
+    def acquire_write(self):
+        with self.lock:
+            if self.DEBUG:
+                me = threading.currentThread()
+                assert me not in self.active_readers, 'This thread already has read access - release that before acquiring write access!'
+                assert me != self.active_writer, 'This thread already has write access and this lock isn\'t writer-reentrant!'
+            self.writer_count += 1
+            if self.active_reader_count:
+                self.readers_finished_cond.wait()
+                while self.active_reader_count:
+                    self.readers_finished_cond.wait()
+
+        self.active_writer_lock.acquire()
+        if self.DEBUG:
+            self.active_writer = me
+
+    def release_write(self):
+        if not self.DEBUG:
+            self.active_writer_lock.release()
+        with self.lock:
+            if self.DEBUG:
+                me = threading.currentThread()
+                assert me == self.active_writer, 'Trying to release write access when it hasn\'t been acquired by this thread!'
+                self.active_writer = None
+                self.active_writer_lock.release()
+            assert self.writer_count > 0
+            self.writer_count -= 1
+            if not self.writer_count and self.waiting_reader_count:
+                self.writers_finished_cond.notifyAll()
+
+    def get_state(self):
+        with self.lock:
+            return (self.writer_count, self.waiting_reader_count, self.active_reader_count)
